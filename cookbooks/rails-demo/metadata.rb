0c7bb7a6e749e585e3ae83b01170c8f4c004a918
Giriraj Singh Gujar <giriraj.gujar@target.com>
2015-01-07 06:59:28 +0530

    Created a demo file

---
+++ cookbooks/rails-demo/metadata.rb
@@ -0,0 +1,9 @@
+name             'rails-demo'
+maintainer       'Giriraj'
+maintainer_email 'giriraj_2k5@yahoo.com'
+license          'All rights reserved'
+description      'Installs/Configures rails-demo'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+depends		 "application"
+depends		 "application_ruby"
